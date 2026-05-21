aff8b95467464616018ac77810dd181131a1e034
Sayed <muhammadsaye741@gmail.com>
2026-05-21 17:27:15 +0600

    Implement firebase auth

--- src/Layout/AuthLayout.jsx
+++ src/Layout/AuthLayout.jsx
@@ -1,19 +1,24 @@
 // import React from 'react';
-
-import { BsArrowLeft } from "react-icons/bs";
 import { Link, Outlet } from "react-router";
+import { BsArrowLeft } from "react-icons/bs";
 
 const AuthLayout = () => {
   return (
-    <div>
-      <Link
-        to="/"
-        className="absolute top-6 left-6 flex items-center gap-2 text-sky-500 hover:underline"
-      >
-        <BsArrowLeft className="text-lg" />
-        <span className="font-medium">Back to Home</span>
-      </Link>
-      <Outlet></Outlet>
+    <div className="max-w-11/12 mx-auto">
+
+      <div className="mt-5 mb-5">
+        <Link
+          to="/"
+          className="flex items-center gap-2 text-sky-500 hover:underline"
+        >
+          <BsArrowLeft className="text-lg" />
+          <span className="font-medium">Back to Home</span>
+        </Link>
+      </div>
+      
+      <div>
+        <Outlet></Outlet>
+      </div>
     </div>
   );
 };
--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,11 +1,57 @@
 // import React from 'react';
 
+import { useForm } from "react-hook-form";
+
 const Login = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const handleRegister = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit(handleRegister)}>
+        <fieldset className="fieldset">
+          {/* --Email-- */}
+          <label className="label">Email</label>
+          <input
+            {...register("email", { required: "Email is required" })}
+            type="email"
+            className="input"
+            placeholder="Email"
+          />
+          {errors.email && (
+            <p className="text-xs text-red-500 mt-1">{errors.email.message}</p>
+          )}
+          {/* ---Password--- */}
+          <label className="label">Password</label>
+          <input
+            {...register("password", { required: "Password is required" ,minLength:6})}
+            type="password"
+            className="input"
+            placeholder="Password"
+          />
+          {errors.password && (
+            <p className="text-xs text-red-500 mt-1">
+              {errors.password.message}
+            </p>
+          )}
+          {
+            errors.password?.type==='minLength' && <p className="text-xs text-red-500 mt-1">
+              Password must be 6 character or long
+            </p>
+          }
+          {/* ---------- */}
+          <button className="btn btn-neutral mt-4">Register</button>
+        </fieldset>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
